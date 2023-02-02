737932e96ce1bc635575946ebc425af9a4ebef0c
teenabhatia <63713718+teenabhatia@users.noreply.github.com>
2023-02-02 16:21:29 -0500

    banner7

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,23 +1,4 @@
 import * as React from "react"
-import Image from 'src/components/Image';
-
-const Sunset = () => {
-  return (
-    <>
-      <p>Sunset Image</p>
-      <Image
-        src="banner.png"
-        className="mx-auto shadow-xl"
-        alt="Banner Image"
-        style={{
-          border: '10px solid green',
-        }}
-      />
-    </>
-  );
-};
-
-export default Sunset;
 
 const pageStyles = {
   color: "#232128",
