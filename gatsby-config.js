c317da307af46d9420cdacba0bb061967b61d441
teenabhatia <63713718+teenabhatia@users.noreply.github.com>
2023-02-02 15:59:59 -0500

    banner5

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,14 +13,14 @@ module.exports = {
         // The unique name for each instance
         name: `pages`,
         // Path to the directory
-        path: `${pennapps}/src/pages/`,
+        path: `${'pennapps'}/src/pages/`,
       },
     },
     {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `data`,
-        path: `${pennapps}/src/data/`,
+        path: `${'pennapps'}/src/images/`,
         // Ignore files starting with a dot
         ignore: [`**/\.*`],
         // Use "mtime" and "inode" to fingerprint files (to check if file has changed)
