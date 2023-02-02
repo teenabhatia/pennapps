a6749c039601e64e0aefbbe5bccac2af7a909090
teenabhatia <63713718+teenabhatia@users.noreply.github.com>
2023-02-02 16:01:07 -0500

    banner6

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,25 +7,6 @@ module.exports = {
     siteUrl: `https://www.yourdomain.tld`,
   },
   plugins: [
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        // The unique name for each instance
-        name: `pages`,
-        // Path to the directory
-        path: `${'pennapps'}/src/pages/`,
-      },
-    },
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `data`,
-        path: `${'pennapps'}/src/images/`,
-        // Ignore files starting with a dot
-        ignore: [`**/\.*`],
-        // Use "mtime" and "inode" to fingerprint files (to check if file has changed)
-        fastHash: true,
-      },
-    },
+
   ],
 }
