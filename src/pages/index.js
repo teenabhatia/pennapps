7478ac8eb5b6259e6c2ebeea82936dc4d4fdbee2
teenabhatia <63713718+teenabhatia@users.noreply.github.com>
2023-02-03 12:00:23 -0500

    new2

--- src/pages/index.js
+++ src/pages/index.js
@@ -80,51 +80,51 @@ const badgeStyle = {
   lineHeight: 1,
 }
 
-// const links = [
-//   {
-//     text: "Tutorial",
-//     url: "https://www.gatsbyjs.com/docs/tutorial/",
-//     description:
-//       "A great place to get started if you're new to web development. Designed to guide you through setting up your first Gatsby site.",
-//     color: "#E95800",
-//   },
-//   {
-//     text: "How to Guides",
-//     url: "https://www.gatsbyjs.com/docs/how-to/",
-//     description:
-//       "Practical step-by-step guides to help you achieve a specific goal. Most useful when you're trying to get something done.",
-//     color: "#1099A8",
-//   },
-//   {
-//     text: "Reference Guides",
-//     url: "https://www.gatsbyjs.com/docs/reference/",
-//     description:
-//       "Nitty-gritty technical descriptions of how Gatsby works. Most useful when you need detailed information about Gatsby's APIs.",
-//     color: "#BC027F",
-//   },
-//   {
-//     text: "Conceptual Guides",
-//     url: "https://www.gatsbyjs.com/docs/conceptual/",
-//     description:
-//       "Big-picture explanations of higher-level Gatsby concepts. Most useful for building understanding of a particular topic.",
-//     color: "#0D96F2",
-//   },
-//   {
-//     text: "Plugin Library",
-//     url: "https://www.gatsbyjs.com/plugins",
-//     description:
-//       "Add functionality and customize your Gatsby site or app with thousands of plugins built by our amazing developer community.",
-//     color: "#8EB814",
-//   },
-//   {
-//     text: "Build and Host",
-//     url: "https://www.gatsbyjs.com/cloud",
-//     badge: true,
-//     description:
-//       "Now you’re ready to show the world! Give your Gatsby site superpowers: Build and host on Gatsby Cloud. Get started for free!",
-//     color: "#663399",
-//   },
-// ]
+const links = [
+  {
+    text: "Tutorial",
+    url: "https://www.gatsbyjs.com/docs/tutorial/",
+    description:
+      "A great place to get started if you're new to web development. Designed to guide you through setting up your first Gatsby site.",
+    color: "#E95800",
+  },
+  {
+    text: "How to Guides",
+    url: "https://www.gatsbyjs.com/docs/how-to/",
+    description:
+      "Practical step-by-step guides to help you achieve a specific goal. Most useful when you're trying to get something done.",
+    color: "#1099A8",
+  },
+  {
+    text: "Reference Guides",
+    url: "https://www.gatsbyjs.com/docs/reference/",
+    description:
+      "Nitty-gritty technical descriptions of how Gatsby works. Most useful when you need detailed information about Gatsby's APIs.",
+    color: "#BC027F",
+  },
+  {
+    text: "Conceptual Guides",
+    url: "https://www.gatsbyjs.com/docs/conceptual/",
+    description:
+      "Big-picture explanations of higher-level Gatsby concepts. Most useful for building understanding of a particular topic.",
+    color: "#0D96F2",
+  },
+  {
+    text: "Plugin Library",
+    url: "https://www.gatsbyjs.com/plugins",
+    description:
+      "Add functionality and customize your Gatsby site or app with thousands of plugins built by our amazing developer community.",
+    color: "#8EB814",
+  },
+  {
+    text: "Build and Host",
+    url: "https://www.gatsbyjs.com/cloud",
+    badge: true,
+    description:
+      "Now you’re ready to show the world! Give your Gatsby site superpowers: Build and host on Gatsby Cloud. Get started for free!",
+    color: "#663399",
+  },
+]
 
 const IndexPage = () => {
   return (
@@ -135,10 +135,10 @@ const IndexPage = () => {
         {/* <span style={headingAccentStyles}>— you just made a Gatsby site! 🎉🎉🎉</span> */}
       </h1>
       <img src={Banner} alt="Banner" class="center"/>
-      <p style={paragraphStyles}>
+      {/* <p style={paragraphStyles}>
         Edit <code style={codeStyles}>src/pages/index.js</code> to see this page
         update in real-time. 😎
-      </p>
+      </p> */}
       <ul style={listStyles}>
         <li style={docLinkStyle}>
           <a
