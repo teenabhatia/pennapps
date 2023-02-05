07a7e7b996a2af3efde1843a9ddbf1fb45cb31ee
teenabhatia <63713718+teenabhatia@users.noreply.github.com>
2023-02-05 16:51:20 -0500

    Add files via upload

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,14 +1,38 @@
-/**
- * @type {import('gatsby').GatsbyConfig}
- */
+// support for .env, .env.development, and .env.production
+require("dotenv").config()
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+})
+
 module.exports = {
   siteMetadata: {
-    title: `pennapps`,
-    siteUrl: `https://www.yourdomain.tld`,
+    siteUrl: "https://gatsbywordpresshomepage.gatsbyjs.io/",
+    title: "Gatsby WordPress Homepage Starter",
+    author: `Gatsby`,
+    description: "A Gatsby Starter for building homepages with WordPress",
   },
   plugins: [
-    "gatsby-plugin-image",
+    {
+      resolve: "gatsby-source-wordpress",
+      options: {
+        url: process.env.WPGRAPHQL_URL,
+      },
+    },
     "gatsby-plugin-sharp",
+    "gatsby-plugin-image",
+    "gatsby-transformer-sharp",
+    "gatsby-plugin-vanilla-extract",
+    {
+      resolve: "gatsby-plugin-manifest",
+      options: {
+        name: "Gatsby Starter WordPress Homepage",
+        short_name: "Gatsby",
+        start_url: "/",
+        // These can be imported once ESM support lands
+        background_color: "#ffffff",
+        theme_color: "#004ca3",
+        icon: "src/favicon.png",
+      },
+    },
   ],
-  
 }
