3ce90171099c6ed943bab3db3ba4b5060ee2bd2c
teenabhatia <63713718+teenabhatia@users.noreply.github.com>
2023-02-06 01:06:36 -0500

    Add files via upload

---
+++ gatsby-config.js
@@ -0,0 +1,11 @@
+const path = require(`path`);
+module.exports = {
+  siteMetadata: {
+    title: `pennapps`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+  ],
+}
